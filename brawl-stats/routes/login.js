2db0e3cd92fcb3c56a6b62f2059b5e19cc87e798
ArthurDev-A <arthurdearaujocustodio0Wgmail.com>
2025-03-03 21:45:44 -0300

    consertanto identação

--- brawl-stats/routes/login.js
+++ brawl-stats/routes/login.js
@@ -9,13 +9,13 @@ const client = new MongoClient(uri);
 const mydb = client.db(process.env.NAME_DB).collection('users');
 
 router.get('/', function(req, res) {
-  res.render('login');
+     res.render('login');
 });
 
 router.post('/', async (req, res) => {
-  const { email, senha } = req.body;
-  const result = await usersController.loginUser(mydb, email, senha);
-  res.send(result.message);
+    const { email, senha } = req.body;
+    const result = await usersController.loginUser(mydb, email, senha);
+    res.send(result.message);
 });
 
 module.exports = router;
